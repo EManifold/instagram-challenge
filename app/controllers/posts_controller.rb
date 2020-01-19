830f60deb270f2a523ac06d672e7f4d3cb7f1f89
Ellie <elliemmanifold@gmail.com>
2020-01-19 19:11:48 +0000

    Add username column to db and username input on sign up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,20 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params) ? (redirect_to posts_url) : (render 'edit')
+  end
+
   private
 
   def post_params
